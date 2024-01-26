002b2bbef259844bbdfc27e45c1b9bae13c547b1
adl.fahmia <adl.fahmia@xl.co.id>
2024-01-26 14:55:13 +0700

    test metadata

--- src/app/invite/kule-gita/page.tsx
+++ src/app/invite/kule-gita/page.tsx
@@ -10,34 +10,34 @@ import { QueryClientProvider, queryClient } from "../../../../react-query";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 
-const kafka = new Kafka({
-  clientId: "next",
-  brokers: ["128.199.143.112:39092"],
-});
+// const kafka = new Kafka({
+//   clientId: "next",
+//   brokers: ["128.199.143.112:39092"],
+// });
 
-const consumer = kafka.consumer({
-  groupId: "qa-group",
-});
-const run = async function (cb: (v: string) => void) {
-  await consumer.connect();
-  await consumer.subscribe({ topic: "quickstart-events", fromBeginning: true });
+// const consumer = kafka.consumer({
+//   groupId: "qa-group",
+// });
+// const run = async function (cb: (v: string) => void) {
+//   await consumer.connect();
+//   await consumer.subscribe({ topic: "quickstart-events", fromBeginning: true });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value?.toString(),
-      });
+//   await consumer.run({
+//     eachMessage: async ({ topic, partition, message }) => {
+//       console.log({
+//         partition,
+//         offset: message.offset,
+//         value: message.value?.toString(),
+//       });
 
-      const v = message.value?.toString();
+//       const v = message.value?.toString();
 
-      if (v) {
-        cb(v);
-      }
-    },
-  });
-};
+//       if (v) {
+//         cb(v);
+//       }
+//     },
+//   });
+// };
 
 export default function Page() {
   const [showOpening, setShowOpening] = useState(true);
