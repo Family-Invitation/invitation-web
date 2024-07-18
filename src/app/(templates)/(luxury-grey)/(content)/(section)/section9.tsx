13dea0a26565b513882e650f8d2441b34dc7e300
Bagas Prasetyadi <bagasprasetyadi@impstudio.co.id>
2024-07-18 21:36:36 +0700

    feat: add vendor

--- src/app/(templates)/(luxury-grey)/(content)/(section)/section9.tsx
+++ src/app/(templates)/(luxury-grey)/(content)/(section)/section9.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import { assetsUrl, urlWA } from "@/lib/constants";
 
 type Props = {};
 
-const section8 = ({ data }: Readonly<any>) => {
+const section9 = ({ data }: Readonly<any>) => {
   return (
     <div
       className={`flex justify-center items-center flex-col h-full w-full bg-center bg-no-repeat bg-cover relative`}
@@ -36,9 +39,52 @@ const section8 = ({ data }: Readonly<any>) => {
             Atas kehadiran dan doa restunya kami ucapkan terimakasih.
           </p>
         </div>
+
+        <div className="absolute bottom-[70px] flex flex-col items-center">
+          <div className="relative flex flex-col items-center">
+            <img
+              src={
+                data.section9.vendor?.picture || assetsUrl + "/logo-brown.png"
+              }
+              alt="logo"
+              style={{
+                // aspectRatio: "1:1",
+                borderRadius: 10,
+                width: "80px",
+                marginTop: "30px",
+              }}
+              onClick={() => {
+                window.open(data.section9.vendor?.url, "_blank");
+              }}
+              data-aos="flip-right"
+            />
+            <div
+              className="text-white mt-3 text-xs font-combo"
+              // data-aos="flip-right"
+            >
+              {data.section9.vendor?.name || "Family Invitation"}
+            </div>
+          </div>
+          {!data.section9.vendor && (
+            <div
+              style={{ display: "flex", marginTop: "20px", gap: "10px" }}
+              data-aos="zoom-in"
+            >
+              <Link href={urlWA + "6289516789565"} target="_blank">
+                <FaWhatsapp size={30} color="#B87A54" />
+              </Link>
+              <Link
+                href="https://www.instagram.com/familyinvitation_/"
+                target="_blank"
+              >
+                <FaInstagram size={30} color="#B87A54" />
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
-export default section8;
+export default section9;
