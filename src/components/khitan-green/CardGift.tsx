998126a799b648b774a3fb9d354f8c8158a22dc7
adl.fahmia <adl.fahmia@xl.co.id>
2024-08-08 11:37:48 +0700

    alert

--- src/components/khitan-green/CardGift.tsx
+++ src/components/khitan-green/CardGift.tsx
@@ -1,4 +1,5 @@
 import { FaRegCopy } from "react-icons/fa";
+import { ToastContainer, toast, Zoom } from "react-toastify";
 
 type RekeningItem = {
   name: string;
@@ -9,28 +10,36 @@ type RekeningItem = {
 const CardGift = ({ name, no, title }: RekeningItem) => {
   const handleCopy = (textToCopy: string) => {
     navigator.clipboard.writeText(textToCopy);
+    toast.success("Nomor rekening berhasil disalin", {
+      position: "top-center",
+      autoClose: 5000,
+      transition: Zoom,
+    });
   };
 
   return (
-    <div
-      className="rounded-lg py-6 px-10 bg-[#f1f1f1] flex justify-center items-center flex-col shadow-md shadow-black"
-      data-aos="flip-up"
-    >
-      <div className="border-t-2 border-[2px] border-[#FFCB58] w-full mb-3"></div>
-      <h3 className="text-2xl text-[#2D2D2D] mb-5 font-shippory">Rekening</h3>
-      <p className="text-[#303030] font-semibold font-poppins text-sm">
-        {title}
-      </p>
-      <p className="text-[#292929] font-poppins text-[15px]">{no}</p>
-      <button
-        className="flex items-center justify-center space-x-2 bg-[#FFCB58]  text-white py-2 px-4 rounded-md mt-4 min-w-[150px] btn-sm"
-        onClick={() => handleCopy(no)}
+    <>
+      <div
+        className="rounded-lg py-6 px-10 bg-[#f1f1f1] flex justify-center items-center flex-col shadow-md shadow-black"
+        data-aos="flip-up"
       >
-        <FaRegCopy className="w-4 h-4" />
-        <span className="font-poppins text-white">Salin Rekening</span>
-      </button>
-      <p className="mt-3 text-[#292929] font-alata">{name}</p>
-    </div>
+        <div className="border-t-2 border-[2px] border-[#FFCB58] w-full mb-3"></div>
+        <h3 className="text-2xl text-[#2D2D2D] mb-5 font-shippory">Rekening</h3>
+        <p className="text-[#303030] font-semibold font-poppins text-sm">
+          {title}
+        </p>
+        <p className="text-[#292929] font-poppins text-[15px]">{no}</p>
+        <button
+          className="flex items-center justify-center space-x-2 bg-[#FFCB58]  text-white py-2 px-4 rounded-md mt-4 min-w-[150px] btn-sm"
+          onClick={() => handleCopy(no)}
+        >
+          <FaRegCopy className="w-4 h-4" />
+          <span className="font-poppins text-white">Salin Rekening</span>
+        </button>
+        <p className="mt-3 text-[#292929] font-alata">{name}</p>
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
